77981dd7a0d59608cde5367fe927744beae92fb0
OlesyaShmanova <olesyapatrikeevna13@yandex.ru>
2022-06-15 00:34:19 +0300

    Правки3.Добавляет функции

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
 /*
 взято https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
+
 function getRandomNumber (min, max) {
-  if (max <= min) {
-    return false;
+  const minimum = Math.min(min, max);
+  const maximum = Math.max(min, max);
+  if (minimum == maximum) {
+    return maximum;
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(minimum);
+  max = Math.floor(maximum);
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 }
 getRandomNumber (4, 13);
-
+getRandomNumber (10, 7));
+getRandomNumber (13, 13);
 
 /*
 взято http://kodesource.top/javascript/form/string-length.php
 */
 
-function lengthString(inputText, maxlength)
-{
+function lengthString(inputText, maxlength) {
   const userInput = inputText.value;
   if (userInput.length <= maxlength)
   {
-    return true;
-  }
-  else
-  {
-    return false;
+    return userInput.length <= maxlength;
   }
 }
 lengthString (document.querySelector('.text__hashtags'), 10);
